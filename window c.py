ac3d40c54a1282559e24cfe6f5744aa4bfe0ab01
sun12yyds <xiaoyang18716576699@outlook.com>
2024-11-09 15:58:40 +0800

    Add files via upload

---
+++ window c.py
@@ -0,0 +1,10 @@
+# 按钮显示内容
+bvalue = ['C', '+', '-', '//', '2', '0', '1', '√', '3', '4', '5', '*', '6', '7', '8', '.', '9', '/', '**', '=']
+index = 0
+# 将按钮进行 5x4 放置
+for row in range(5):
+    for col in range(4):
+        d = bvalue[index]
+        index += 1
+        btnDigit = tkinter.Button(tk, text=d, command=lambda x=d: onclick(x))
+        btnDigit.place(x=20 + col * 70, y=50 + row * 30, width=50, height=20)
